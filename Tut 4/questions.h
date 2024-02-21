3758a7994b0c49ebf0a9092a29c2b75f53486973
officialhorizon <hanumagaji@gmail.com>
2024-02-20 22:28:11 -0500

    changes to questions.h

--- Tut 4/questions.h
+++ Tut 4/questions.h
@@ -18,9 +18,9 @@
 
 // List of 3 categories as array of strings
 static char categories[NUM_CATEGORIES][MAX_LEN] = {
-    "programming", 
-    "algorithms", 
-    "databases"
+    "Math", 
+    "Programming", 
+    "Soccer"
 };
 
 // Questions struct for each question
