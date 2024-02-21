cff0cf46170ef43b6628812e14b892581353515a
Hanum Magaji <58070743+hanum-magaji@users.noreply.github.com>
2024-02-20 22:49:12 -0500

    Update players.c

--- Tut 4/players.c
+++ Tut 4/players.c
@@ -1,10 +1,4 @@
-/*
- * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2015, <GROUP MEMBERS>
- * All rights reserved.
- *
- */
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +7,12 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
+    for(int x = 0; x < num_players; x++){
+        if(strcmp(players[x].name,name)==0)
+        {
+            return true
+        }
+    }
     return false;
 }
 
@@ -20,5 +20,12 @@ bool player_exists(player *players, int num_players, char *name)
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
+    for(int x = 0; x < num_players; x++)
+    {
+            if(strcmp(players[x].name,name)==0)
+            {
+                players[x].score = players[x].score + score;
+            }
+    }
     
 }
